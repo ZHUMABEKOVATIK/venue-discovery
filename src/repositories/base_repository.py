d7ac9c9538474ba42b9c344e4974d23f7a0c00f5
Atabek <atabek.atik.808@google.com>
2026-06-29 03:44:07 +0500

    some things fixed

--- src/repositories/base_repository.py
+++ src/repositories/base_repository.py
@@ -17,13 +17,19 @@ class BaseRepository(Generic[T]):
         await self.session.flush()
         return obj
     
+    async def get_by_id(self, id: int) -> T | None:
+        return await self.session.scalar(
+            select(self.model).where(self.model.id == id)
+        )
+    
+    async def get_all(self) -> list[T]:
+        return (await self.session.scalars(
+            select(self.model)
+            .order_by(self.model.id.asc())
+        )).all()
+    
     async def update(self, id: int, **kwargs) -> T | None:
-        data = (
-            await self.session.execute(
-                select(self.model)
-                .where(self.model.id == id)
-            )
-        ).scalar_one_or_none()
+        data = await self.get_by_id(id)
 
         if data is None:
             return None
@@ -39,46 +45,29 @@ class BaseRepository(Generic[T]):
         await self.session.delete(obj)
         await self.session.flush()
     
-    async def get_by_id(self, id: int) -> T | None:
-        return await self.session.scalar(
-            select(self.model).where(self.model.id == id)
-        )
-    
     async def get_total(self) -> int:
         return await self.session.scalar(
             select(func.count(self.model.id))
         )
     
 class SoftDeleteRepository(BaseRepository[SDT]):
-    async def update(self, id: int, **kwargs) -> SDT | None:
-        data = (
-            await self.session.execute(
-                select(self.model)
-                .where(self.model.id == id)
-                .where(self.model.is_deleted.is_(False))
-            )
-        ).scalar_one_or_none()
-
-        if data is None:
-            return None
-
-        for key, value in kwargs.items():
-            if hasattr(data, key):
-                setattr(data, key, value)
-        
-        await self.session.flush()
-        return data
-
-    async def delete(self, obj: SDT) -> None:
-        obj.is_deleted = True
-        await self.session.flush()
-
     async def get_by_id(self, id: int) -> SDT | None:
         return await self.session.scalar(
             select(self.model)
             .where(self.model.id == id)
             .where(self.model.is_deleted.is_(False))
         )
+    
+    async def get_all(self) -> list[SDT]:
+        return (await self.session.scalars(
+            select(self.model)
+            .where(self.model.is_deleted.is_(False))
+            .order_by(self.model.id.asc())
+        )).all()
+
+    async def delete(self, obj: SDT) -> None:
+        obj.is_deleted = True
+        await self.session.flush()
 
     async def get_total(self) -> int:
         return await self.session.scalar(
